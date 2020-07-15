eb4c728191323bea4fe6d2605f2fdc10ac57ed08
Cristhian Guevara <cristhianguevara1311@gmail.com>
2020-07-15 10:26:12 -0500

    Primer commit

---
+++ CimMVC/Models/Incidencia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CimMVC.Models
+{
+    public class Incidencia
+    {
+        public int Id;
+        public string CodigoIncidencia;
+        public string TipoIncidencia;
+        public string Turno;
+        public string Dni;
+        public string Distrito;
+        public string CodigoContacto;
+    }
+
+    public class IncidenciaDbContext : DbContext { 
+    
+        public DbSet<Incidencia> Incidencias { get; set; }
+    
+    }
+}
---
+++ CimMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CimMVC.Startup))]
+namespace CimMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
